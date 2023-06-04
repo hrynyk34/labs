2ff615594b8c7dbeab9d098dd8c0c853acbe42bd
hrynyk34 <130141847+hrynyk34@users.noreply.github.com>
2023-06-04 18:06:38 +0300

    pairs.py

--- Лабораторна робота #3/pairs.py
+++ Лабораторна робота #3/pairs.py
@@ -1,12 +1,20 @@
-def find_pairs(numbers):
-    pairs = []
-    for i in range(len(numbers)):
-        for j in range(i+1, len(numbers)):
-            if numbers[i] + numbers[j] == 10:
-                pairs.append((numbers[i], numbers[j]))
-    return pairs
+from itertools import combinations
 
-numbers = [1, 2, 3, 4, 5, 5, 6, 7]
-pairs = find_pairs(numbers)
-for pair in pairs:
-    print(f"{pair[0]} + {pair[1]}")
+def select(list_1, list_2):
+    list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    counter = 0
+    numbers = list_1 + list_2
+    pairs = []
+    for pair in combinations(numbers, 2):
+        if sum(pair) == 10:
+            pairs.append(pair)
+
+    if len(pairs) > 0:
+        print('Ваші числа, сума яких рівна 10:')
+        for pair in pairs:
+            print(pair[0], pair[1])
+    else:
+        print('Немає чисел, сума яких рівна 10.')
+
+select([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9])
