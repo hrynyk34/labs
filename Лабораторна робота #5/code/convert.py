1fba38df59a2cd6e2d1ac09d1c280de5e9db4ca9
hrynyk34 <130141847+hrynyk34@users.noreply.github.com>
2023-06-05 12:34:35 +0300

    convert.py

--- Лабораторна робота #5/code/convert.py
+++ Лабораторна робота #5/code/convert.py
@@ -1,5 +1,5 @@
 from num2words import num2words
 
 def convert_number_to_words(number):
-    """Перетворення числа на його представлення словом."""
+    """Перетворення числа на його представлення словом"""
     return num2words(number)
